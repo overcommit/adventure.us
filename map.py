19e6124f5b14c3f3e0653b71627a5963f416bae5
overcommit <austin.c.downing@gmail.com>
2023-04-15 14:45:48 -0700

    layers working

--- map.py
+++ map.py
@@ -100,6 +100,7 @@ if st.button("Find Random Venue"):
 
 
             st.pydeck_chart(pdk.Deck(
+                "IconLayer"
                 map_style="mapbox://styles/mapbox/satellite-streets-v11",
                 initial_view_state={
                     "latitude": selected_venue['latitude'],
